ad38ca49b52d1ddfe9d3a855f664a10a7c13faa6
Swati Narang <swatinarang1225@gmail.com>
2022-12-26 16:14:08 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__, template_folder ='Template')
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
 
